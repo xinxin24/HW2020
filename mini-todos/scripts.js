b81024b64e0fc3cdbbf386b2994c2eac408c67e0
xinxin <xinxin@email.com>
2020-06-15 12:10:23 +0800

    更新一个文件

--- mini-todos/scripts.js
+++ mini-todos/scripts.js
@@ -1,10 +1,54 @@
 
+let tasks = []; //{title: "aaaaa", done: false, important}
 function renderEditor() {
     let inputEl = document.querySelector("#default-todo-panel .todo-editor > input")
 
-    inputEl.onchange = (e) => {
-        console.log("input change: ",e);
+    //inputEl.onchange = (e) => {
+    //  console.log("text, ",e.target.value);
+       // console.log("input change: ",e);
+   // };
+
+   let addEl = document.querySelector("#default-todo-panel .todo-editor > button");
+   addEl.onclick = (e) => {
+    console.log("add click");
+    let newTask = {
+        title: inputEl.value,
+        done: false,
     };
+ 
+    inputEl.value = "";
+
+    tasks.push(newTask);
+    
+    console.log("tasks: ", tasks);
+
+    renderTaskItems();
+ };
+}
+
+function renderTaskItems() {
+    console.log("render items");
+    let itemsEl =  document.querySelector("#default-todo-panel .todo-items");
+    
+    for (let i =0; i< tasks.length; i++) {
+        let task = tasks[i];
+        let itemEl = document.createElement("div");
+
+        let doneEl = document.createElement("input");
+        doneEl.type = "checkbox";
+        itemEl.append(doneEl);
+
+        let titleEl = document.createElement("label");
+        titleEl.innerText = task.title;
+        itemEl.append(titleEl);
+
+        let cancelEl = document.createElement("button");
+        cancelEl.innerText = "X";
+        itemEl.append(cancelEl);
+        itemsEl.append(itemEl);
+      
+    }
 }
 
 renderEditor();
+renderTaskItems();
